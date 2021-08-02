f43bde281baea161b7d1665e6ac10a8ce0346e44
Anthony Co <anthonyashco@gmail.com>
2021-08-02 14:43:38 -0700

    Add constraints to encoding.

--- 2021-08-02/encoded_message.py
+++ 2021-08-02/encoded_message.py
@@ -21,7 +21,17 @@ def coder(query: str) -> str:
         for j in range(1, place + 1):
             if place % j == 0:
                 factors.append((j, place // j))
-        values = random.choice(factors)
+        length = len(factors)
+        # prefer 1 if prime
+        if length <= 2:
+            values = factors[length - 1]
+        # avoid factors of 1 if not prime
+        elif length == 3:
+            values = factors[1]
+        else:
+            factors.pop(length - 1)
+            factors.pop(0)
+            values = random.choice(factors)
         result.append(f"{alpha[values[0]-1]}{values[1]}")
 
     return "".join(result)
